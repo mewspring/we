dea967d7d1e3b5798c1b9624fca8005e9cb75c24
mewmew <rnd0x00@gmail.com>
2014-02-04 17:00:49 +0100

    we: Add Event type.

--- we.go
+++ we.go
@@ -9,3 +9,31 @@ type Resize struct {
 	Width  int
 	Height int
 }
+
+// An Event represents a window, keyboard or mouse event.
+//
+// Window events
+//
+// The following window events are defined by this package.
+//    http://godoc.org/github.com/mewmew/we#Close
+//    http://godoc.org/github.com/mewmew/we#Resize
+//
+// Keyboard events
+//
+// The following keyboard events are defined by this package.
+//    http://godoc.org/github.com/mewmew/we/#KeyPress
+//    http://godoc.org/github.com/mewmew/we/#KeyRelease
+//    http://godoc.org/github.com/mewmew/we/#KeyRepeat
+//    http://godoc.org/github.com/mewmew/we/#KeyRune
+//
+// Mouse events
+//
+// The following mouse events are defined by this package.
+//    http://godoc.org/github.com/mewmew/we/#MousePress
+//    http://godoc.org/github.com/mewmew/we/#MouseRelease
+//    http://godoc.org/github.com/mewmew/we/#MouseMove
+//    http://godoc.org/github.com/mewmew/we/#MouseDrag
+//    http://godoc.org/github.com/mewmew/we/#MouseEnter
+//    http://godoc.org/github.com/mewmew/we/#ScrollX
+//    http://godoc.org/github.com/mewmew/we/#ScrollY
+type Event interface{}
